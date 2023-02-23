4dec5529b897a6043bbe5f1e46ccc75c3234b20c
Kaleidio <christhefennec@gmail.com>
2023-02-23 20:02:55 +0000

    change bullet balance
    prepares for headshot changes to DPS balance

--- src/main/java/xyz/kaleidiodev/kaleidiosguns/config/KGConfig.java
+++ src/main/java/xyz/kaleidiodev/kaleidiosguns/config/KGConfig.java
@@ -999,19 +999,19 @@ public class KGConfig {
         builder.push("bullet_config");
         flintBulletDamage = builder
                 .comment("Configure the damage of Flint Bullets.  This tier should always be minimal damage")
-                .defineInRange("flintBulletDamage", 5D, 1D, 20D);
+                .defineInRange("flintBulletDamage", 4D, 1D, 20D);
         ironBulletDamage = builder
                 .comment("Configure the damage of Iron Bullets")
-                .defineInRange("ironBulletDamage", 5.5D, 1D, 20D);
+                .defineInRange("ironBulletDamage", 5D, 1D, 20D);
         blazeBulletDamage = builder
                 .comment("Configure the damage of Blaze Bullets.  This tier should always be the middle ground in damage total including flames.")
                 .defineInRange("blazeBulletDamage", 6D, 1D, 20D);
         hungerBulletDamage = builder
                 .comment("Configure the damage of Hunger Bullets")
-                .defineInRange("hungerBulletDamage", 6.5D, 1D, 20D);
+                .defineInRange("hungerBulletDamage", 7D, 1D, 20D);
         xpBulletDamage = builder
                 .comment("Configure the damage of XP Bullets.  This tier should always be the highest tier of damage.")
-                .defineInRange("xpBulletDamage", 7.5D, 1D, 20D);
+                .defineInRange("xpBulletDamage", 8D, 1D, 20D);
         hungerBulletDurability = builder
                 .comment("Configure the durability of Hunger Bullets.")
                 .defineInRange("hungerBulletDurability", 72, 1, 32767);
