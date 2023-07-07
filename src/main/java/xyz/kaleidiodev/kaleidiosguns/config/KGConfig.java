6604651d48e19fab6064cd82f811ea2d2d2bcabd
Kaleidio <christhefennec@gmail.com>
2023-07-07 14:22:36 +0100

    disable hero wave shotgun config until further notice

--- src/main/java/xyz/kaleidiodev/kaleidiosguns/config/KGConfig.java
+++ src/main/java/xyz/kaleidiodev/kaleidiosguns/config/KGConfig.java
@@ -653,7 +653,7 @@ public class KGConfig {
                 .defineInRange("spreadgunMaximumSpeed", 2.0D, 0.1D, 10D);
         builder.pop();
 
-        builder.push("hero wave shotgun");
+        builder.push("hero wave shotgun !!!CURRENTLY DISABLED!!!");
         heroShotgunDamageMultiplier = builder
                 .comment("Define the Damage multiplier for Hero Wave Shotguns")
                 .defineInRange("heroShotgunDamageMultiplier", 0.6D, 0.1D, 5D);
